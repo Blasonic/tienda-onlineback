cd555e345330960979b2f724f95c136bb91d467f
Blasonic <168122520+Blasonic@users.noreply.github.com>
2024-11-20 13:28:20 +0100

    cmabio de rutas en el app.js

--- app.js
+++ app.js
@@ -2,33 +2,33 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-const usuariosRoutes = require('./rutas/usuarios');
-const productosRoutes = require('./rutas/productos');
-const pedidosRoutes = require('./rutas/pedidos');
-const detallesPedidosRoutes = require('./rutas/detallesPedidos');
-const carritosRoutes = require('./rutas/carritos');
-const carritoProductosRoutes = require('./rutas/carritoProductos');
-const pagosRoutes = require('./rutas/pagos');
-const enviosRoutes = require('./rutas/envios');
+const rutaUsuarios = require('./rutas/usuarios');
+const rutaProductos = require('./rutas/productos');
+const rutaPedidos = require('./rutas/pedidos');
+const rutaDetallesPedidos = require('./rutas/detallesPedidos');
+const rutacarrito = require('./rutas/carritos');
+const rutaCarritoProductos = require('./rutas/carritoProductos');
+const rutaPagos = require('./rutas/pagos');
+const rutaEnvios = require('./rutas/envios');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Middlewares
+
 app.use(cors());
 app.use(bodyParser.json());
 
-// Rutas
-app.use('/usuarios', usuariosRoutes);
-app.use('/productos', productosRoutes);
-app.use('/pedidos', pedidosRoutes);
-app.use('/detalles-pedidos', detallesPedidosRoutes);
-app.use('/carritos', carritosRoutes);
-app.use('/carrito-productos', carritoProductosRoutes);
-app.use('/pagos', pagosRoutes);
-app.use('/envios', enviosRoutes);
 
-// Inicio del servidor
+app.use('/usuarios', rutaUsuarios);
+app.use('/productos', rutaProductos);
+app.use('/pedidos', rutaPedidos);
+app.use('/detalles-pedidos', rutaDetallesPedidos);
+app.use('/carritos', rutacarrito);
+app.use('/carrito-productos', rutaCarritoProductos);
+app.use('/pagos', rutaPagos);
+app.use('/envios', rutaEnvios);
+
+
 app.listen(port, () => {
     console.log(`Servidor corriendo en http://localhost:${port}`);
 });

b3ff7fc14bdc3771e789bbd8cfafda956b5be6e0
MarcosGonzFer <marcos0gon@gmail.com>
2025-02-18 09:32:16 +0100

    cambios para verificacion

--- rutas/productos.js
+++ rutas/productos.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../bd'); 
 
+// Ruta para obtener todos los productos
 router.get('/', async (req, res) => {
     try {
         const [productos] = await db.query('SELECT * FROM zapatillas');
@@ -11,6 +12,8 @@ router.get('/', async (req, res) => {
         res.status(500).json({ error: 'Error en el servidor' });
     }
 });
+
+
 router.get('/:id', async (req, res) => {
     try {
         const { id } = req.params;
@@ -26,4 +29,21 @@ router.get('/:id', async (req, res) => {
         res.status(500).json({ error: 'Error en el servidor' });
     }
 });
-module.exports = router; // 
+
+router.get('/verificacion/:verification', async (req, res) => {
+    try {
+        const { verification } = req.params;
+        const [producto] = await db.query('SELECT * FROM zapatillas WHERE verification = ?', [verification]);
+
+        if (producto.length === 0) {
+            return res.status(404).json({ error: 'Producto no encontrado' });
+        }
+
+        res.json(producto[0]);
+    } catch (error) {
+        console.error('Error al obtener el producto:', error);
+        res.status(500).json({ error: 'Error en el servidor' });
+    }
+});
+
+module.exports = router;
